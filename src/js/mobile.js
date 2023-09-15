aa541e85dca503553153df978979df1d0eeebef5
Olesya <krayanskaya_ov@mail.ru>
2023-09-15 13:20:24 +0300

    media queries mobile

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -4,15 +4,21 @@ const mobileFooter = document.querySelector('.mobile__footer');
 const burgerIcon = document.querySelector('.burger__icon');
 import close from '../assets/images/Close.svg';
 import menu from '../assets/images/menu.svg';
+const mobile = document.querySelector('.mobile');
+const mobileHeader = document.querySelector('.mobile__header');
 
 burgerBtn.addEventListener('click', () => {
   if (burgerIcon.src == menu) {
     burgerIcon.src = close;
     mobileMenu.style.display = 'flex';
     mobileFooter.style.display = 'block';
+    mobile.style.paddingBottom = '92px';
+    mobileHeader.style.marginBottom = '40px';
   } else {
     burgerIcon.src = menu;
     mobileMenu.style.display = 'none';
     mobileFooter.style.display = 'none';
+    mobile.style.paddingBottom = '10px';
+    mobileHeader.style.marginBottom = '0';
   }
 });
